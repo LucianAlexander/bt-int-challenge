484939151e58d6b0ba08802802b75f0237fb5377
Lemnariu Lucian Alexandru <lucianalexandru.lemnariu@eng.it>
2021-09-07 22:45:05 +0200

    min fix

--- src/main/java/bt/group/btintchallenge/controller/BtTransactionSResource.java
+++ src/main/java/bt/group/btintchallenge/controller/BtTransactionSResource.java
@@ -2,9 +2,7 @@ package bt.group.btintchallenge.controller;
 
 import models.Branch;
 import models.Customer;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,7 +19,8 @@ public class BtTransactionSResource {
     private static final String CLASS_NAME = BtTransactionSResource.class.getName();
     private static final Logger logger = Logger.getLogger(CLASS_NAME);
 
-    @RequestMapping("/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    @ResponseBody
     public List<Customer> getListCustommers(@PathVariable(name = "id") Long id) {
         logger.log(Level.INFO, "Richiesta Lista Branches brach_id: {0}", id);
         String sDate1 = "25/09/1989";
@@ -46,8 +45,12 @@ public class BtTransactionSResource {
         } else return null;
     }
 
-    @RequestMapping("/house/{name}")
-    public Branch getHouse(@PathVariable(name = "name") String name) {
-        return new Branch(2L, "Descriere Pe Scurt", "Descriere", "Str Aurel Vlaicu 23, Suceava", 3L);
+    @RequestMapping(method = RequestMethod.GET, value = "/branch/{id}")
+    @ResponseBody
+    public List<Branch> getHouse(@PathVariable(name = "id") Long id) {
+
+        List<Branch> listBranches = new ArrayList<>();
+        listBranches.add(new Branch(2L, "Descriere Pe Scurt", "Descriere", "Str Aurel Vlaicu 23, Suceava", 3L));
+        return listBranches;
     }
 }
